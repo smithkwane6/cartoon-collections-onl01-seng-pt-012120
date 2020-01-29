a6a0dcc9f3e14311f413891fcef6dd3155a70d0f
Kwane Smith <smithkwane6@gmail.com>
2020-01-29 22:00:26 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,5 +1,9 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(array)
+  i = 0
+  while i < array.length
+    puts "#{i + 1}. #{array[i]}"
+    i += 1
+  end
 end
 
 def summon_captain_planet# code an argument here
